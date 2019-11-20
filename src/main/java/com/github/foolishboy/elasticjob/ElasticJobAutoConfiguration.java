8aa1da5041360ce5eeaed5c681fe30ae62cd2105
foolishboy66 <835948617@qq.com>
2019-11-20 23:27:45 +0800

    优化导包

--- src/main/java/com/github/foolishboy/elasticjob/ElasticJobAutoConfiguration.java
+++ src/main/java/com/github/foolishboy/elasticjob/ElasticJobAutoConfiguration.java
@@ -19,9 +19,6 @@ import com.github.foolishboy.elasticjob.config.ZookeeperRegCenterProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -169,4 +166,3 @@ public class ElasticJobAutoConfiguration implements InitializingBean {
         return "".equals(str);
     }
 }
-    
